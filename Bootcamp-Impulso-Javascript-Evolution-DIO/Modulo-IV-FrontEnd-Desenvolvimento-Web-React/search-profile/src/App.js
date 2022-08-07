d150fd3347651ffef44f5a2e34b5c31c2c02cad8
Micael Santos <micaelmaicon@hotmail.com>
2022-08-06 21:44:54 -0300

    refact: Refactor the app component

--- Bootcamp-Impulso-Javascript-Evolution-DIO/Modulo-IV-FrontEnd-Desenvolvimento-Web-React/search-profile/src/App.js
+++ Bootcamp-Impulso-Javascript-Evolution-DIO/Modulo-IV-FrontEnd-Desenvolvimento-Web-React/search-profile/src/App.js
@@ -2,10 +2,30 @@ import Layout from "./components/layout";
 
 const  App = () => {
   return (
-    <section >
-      <Layout />
-      Bem vindo(a) ao projeto search profile com api github
-    </section>
+    <main>
+      <Layout>
+        <div>
+          <img src="https://avatars.githubusercontent.com/u/78963788?v=4" alt="avatar do usuário"/>
+          <h1>Micael Santos</h1>
+          <h3>Username: </h3>
+          <span>Micael-M</span>
+          <div>
+            <h4>Seguidores</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Favoritos</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Seguido</h4>
+            <span>5</span>
+          </div>
+        </div>
+        <div>Repositórios</div>
+        <div>Favoritos</div>
+      </Layout>
+    </main>
   );
 }
 
--- Bootcamp-Impulso-Javascript-Evolution-DIO/Modulo-IV-FrontEnd-Desenvolvimento-Web-React/search-profile/src/components/layout/index.js
+++ Bootcamp-Impulso-Javascript-Evolution-DIO/Modulo-IV-FrontEnd-Desenvolvimento-Web-React/search-profile/src/components/layout/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Layout = ({ children }) => {
   return (
     <section>
-    <p> Olá brasiiiiiil </p>
+    <p> Olá, brasiiiiiil! O componente layout está pegando</p>
     { children }
   </section>
   );
