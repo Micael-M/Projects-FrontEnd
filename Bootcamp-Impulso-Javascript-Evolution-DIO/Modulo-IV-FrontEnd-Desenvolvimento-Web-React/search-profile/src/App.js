c2e4aa7c6d22cd2c1202f25e83ba9ebba5dfe9d7
Micael Santos <micaelmaicon@hotmail.com>
2022-08-14 21:49:14 -0300

    feat: create github-hook

--- Bootcamp-Impulso-Javascript-Evolution-DIO/Modulo-IV-FrontEnd-Desenvolvimento-Web-React/search-profile/src/App.js
+++ Bootcamp-Impulso-Javascript-Evolution-DIO/Modulo-IV-FrontEnd-Desenvolvimento-Web-React/search-profile/src/App.js
@@ -2,12 +2,12 @@ import { ResetCss } from "./components/global/resetCSS";
 import Layout from "./components/layout";
 import ListRepos from "./components/listrepos";
 import Profile from "./components/profile";
-import { GithubContext } from "./provider/github-provider";
+import GithubProvider from "./provider/github-provider";
 
 const App = () => {
   return (
     <main>
-      <GithubContext />
+      <GithubProvider />
       <ResetCss />
       <Layout>
         <Profile />
---
+++ Bootcamp-Impulso-Javascript-Evolution-DIO/Modulo-IV-FrontEnd-Desenvolvimento-Web-React/search-profile/src/hooks/github-hooks.js
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import { GithubContext } from '../provider/github-provider';
+
+const useGithub = () => {
+  const { githubState } = useContext(GithubContext);
+  return { githubState };
+};
+
+export default useGithub;
